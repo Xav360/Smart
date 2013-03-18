3654100f6b5b3889692eddd560bb8aec964a2253
Hugues Pouillot <hpouillot@bitbucket.org>
2013-03-18 17:17:31 +0100

    Adding info panel

--- display.js
+++ display.js
@@ -7,18 +7,43 @@ var edgePath;
 
 function display(json) {
 
-    var myStyle = {
-        "color": "#ff7800",
-        "weight": 5,
-        "opacity": 0.65
-    };
+    var min = Math.floor(json.travelInfo.time/60);
+    var hour = json.travelInfo.time/60;
+    var dist = Math.floor(json.travelInfo.distance/1000);
+
+    $('#travelTime').text(min + " min");
+    $('#travelDistance').text(dist + " km");
+
+    var myIconA = L.icon({
+        iconUrl: 'A-position.png',
+        iconRetinaUrl: 'A-position.png',
+        iconSize: [33, 40],
+        iconAnchor: [16, 40]
+    });
+
+    var myIconB = L.icon({
+        iconUrl: 'B-position.png',
+        iconRetinaUrl: 'B-position.png',
+        iconSize: [33, 40],
+        iconAnchor: [16, 40]
+    });
+
+
 
     var edges = [];
     var pointList = json.steps ;
 
     if (!edgePath==null) {
         edgePath.setMap(null);
+        AMarker=null;
+        BMarker=null;
     }
+
+    var BMarker = new L.marker([pointList[0].lat,pointList[0].lon], {icon : myIconB});
+    BMarker.addTo(map);
+
+    var AMarker = new L.marker([pointList[pointList.length-2].lat,pointList[pointList.length-2].lon], {icon : myIconA});
+    AMarker.addTo(map);
 
     for (var i =0; i<pointList.length-1; i++){
         edges.push(new L.LatLng(pointList[i].lat,pointList[i].lon));
@@ -29,7 +54,14 @@ function display(json) {
         [pointList[pointList.length-1].lat, pointList[pointList.length-1].lon]
     ]);
 
-    L.polyline(edges, {color: 'red'}).addTo(map);
+    var myStyle = {
+        "color": "#1E00FF",
+        "weight": 8,
+        "opacity": 0.70,
+        "smoothFactor":0.1
+    };
+
+    L.polyline(edges, myStyle).addTo(map);
 }
 
 function submitform()
@@ -45,15 +77,14 @@ function addToUrl(index, point) {
 
 function redirect(url, index, point){
     url = url + "&lat"+index+"="+point.lat()+"&lon"+index+"="+point.lng();
-    /*
-     var radios = trajet.transportation;
-     var selectedValue="CAR";
 
-     for(var i = 0; i < radios.length; i++) {
-     if(radios[i].checked) selectedValue = radios[i].value;
-     }
-     */
-    var selectedValue="FOOT";
+    var radios = $('input[name="trans"]');
+    var selectedValue="CAR";
+
+    for(var i = 0; i < radios.length; i++) {
+        if(radios[i].checked) selectedValue = radios[i].value;
+    }
+
     url = url + "&trans=" + selectedValue;
 
     $.getJSON(url, function(json){
